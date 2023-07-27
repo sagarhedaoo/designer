bb624aa6af6f86a1f6abf8c5f0389f6afb4bef44
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-07-27 15:11:50 -0400

    File structure ready

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
+import Shirt from "./Shirt";
+import Backdrop from "./Backdrop";
+import CameraRig from "./CameraRig";
 
-const Canvas = () => {
-  return <div>Canvas</div>;
+const ConvasModel = () => {
+  return <div>ConvasModel</div>;
 };
 
-export default Canvas;
+export default ConvasModel;
