5d51c7a79832a72a6325241e2f46eb29d2021b71
Sagar Hedaoo <sagarhedaoo@gmail.com>
2023-07-27 15:48:54 -0400

    Model, camera, lights and animations done

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -13,6 +13,20 @@ const CameraRig = ({ children }) => {
     const isBreakpoint = window.innerWidth <= 1260;
     const isMobile = window.innerWidth <= 600;
 
+    //set initial position
+    let targetPosition = [-0.4, 0, 2];
+    if (snap.intro) {
+      if (isBreakpoint) targetPosition = [0, 0, 2];
+      if (isMobile) targetPosition = [0, 0.2, 2.5];
+    } else {
+      if (isMobile) targetPosition = [0, 0, 2.5];
+      else targetPosition = [0, 0, 2];
+    }
+
+    //set model camera position
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta);
+
+    //rotation
     easing.dampE(
       group.current.rotation,
       [state.pointer.y / 10, -state.pointer.x / 5, 0],
@@ -20,7 +34,6 @@ const CameraRig = ({ children }) => {
       delta
     );
   });
-  //rotation
 
   return <group ref={group}>{children}</group>;
 };
